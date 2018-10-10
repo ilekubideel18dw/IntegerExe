d09cf4d366fe15a7e5cb12bcd6652edf0adec1ef
ik013043z1 <ik013043z1@H013043.NET>
2018-10-10 09:54:10 +0200

    v1.1

--- src/IntegerExe.java
+++ src/IntegerExe.java
@@ -1,12 +1,12 @@
 public class IntegerExe {
 	public static void main(String args[]) {
 		
-		// We are going to declare two Integers, and we are going to compare with compareTO().
-		Integer num1 = new Integer("14");
-		Integer num2 = new Integer("7");
+		// We are going to declare two Integers, and we are going to compare with compareTo().
+		Integer num1 = 14;
+		Integer num2 = 10;
 		int comp = num1.compareTo(num2);
 		
-		// To know the compare, we use a if. 
+		// To know do the comparison, we use a if. 
 		if(comp > 0) {
 	         System.out.println("num1 is greater than num2");
 	    } else if(comp < 0) {
@@ -15,16 +15,32 @@ public class IntegerExe {
 	         System.out.println("num1 is equal to num2");
 	    }
 		
-		//
+		// getInteger() method determines the integer value of the system property with the specified name.
+		String sgt = "sun.arch.data.model";
+		System.out.println("Value = " + Integer.getInteger(sgt));
 		
 		// intValue() returns the value of this Integer as an int.
 		int iv = num1.intValue();
 	    System.out.println("Value = " + iv);
 	    
-	    // 
+	    // parseInt().
 	    
 	    // shortValue() returns the value of this Integer as an short.
 	    short sv = num1.shortValue();
 	    System.out.println("Value = " + sv);
+	    
+	    // toString().
+	 	String tst = num2.toString(30, 10);
+	    System.out.println("Value = " + tst);
+	 
+	    tst = num2.toString(30, 18);
+	    System.out.println("Value = " + tst);
+
+	    tst = num2.toString(30, 7);
+	    System.out.println("Value = " + tst);
+	    
+	    // valueOf() method returns an Integer object holding the value of the specified String.
+	    String stv = "50";
+	    System.out.println("Value = " + num1.valueOf(stv));
 	}
 }
